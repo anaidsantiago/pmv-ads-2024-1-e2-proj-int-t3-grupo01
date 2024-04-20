13771b74d5c03965fd0f60f4eba5a0d611418f91
João <joaovictor_afl@hotmail.com>
2024-04-19 21:05:55 -0300

    Commit inicial do projeto

---
+++ src/GestLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
