c2ab16d5fccb9da27febfa9526213718e2c048ca
João <joaovictor_afl@hotmail.com>
2024-04-19 21:28:31 -0300

    tst

--- src/GestLab/Controllers/HomeController.cs
+++ src/GestLab/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using GestLab.Data;
 using GestLab.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -21,6 +22,22 @@ namespace GestLab.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public void AddUsuario()
+        {
+            using GestLabContext context = new GestLabContext();
+            UsuarioModel user = new()
+            {
+                Nome = "Test",
+                Senha = "Test",
+                Email = "Test",
+                Telefone = "Test",
+                Tipo = "Test",
+            };
+
+            context.Usuarios.Add(user);
+            context.SaveChanges();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
