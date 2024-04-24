6baacedcf8e235d755b92a893c7c864144d010a0
João <joaovictor_afl@hotmail.com>
2024-04-24 17:24:37 -0300

    Ajustes

--- src/GestLab/Models/UsuarioModel.cs
+++ src/GestLab/Models/UsuarioModel.cs
@@ -1,12 +1,18 @@
-﻿namespace GestLab.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestLab.Models
 {
     public class UsuarioModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "A senha deve ser informada")]
         public string Senha { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
         public string Nome { get; set; }
         public string? Telefone { get; set; }
+        [EmailAddress(ErrorMessage = "Email Invalido")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "O tipo de usuario deve ser informado")]
         public string Tipo { get; set; }
     }
 }
