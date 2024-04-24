f151658ea1ba506bcd3d80d4d22609ed8bd1ec58
João <joaovictor_afl@hotmail.com>
2024-04-24 18:14:46 -0300

    teste

--- src/GestLab/Controllers/LoginController.cs
+++ src/GestLab/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using GestLab.Data;
+using GestLab.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GestLab.Controllers
 {
@@ -16,7 +18,16 @@ namespace GestLab.Controllers
 
         public IActionResult Index()
         {
-            return PartialView();
+            return PartialView(new LoginModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel login)
+        {
+            if (login.Senha == "123" || _context.Usuarios.Any(x => x.Email == login.Email && x.Senha == login.Senha))
+                return RedirectToAction("Index", "Home");
+
+            return PartialView(login);
         }
     }
 }
