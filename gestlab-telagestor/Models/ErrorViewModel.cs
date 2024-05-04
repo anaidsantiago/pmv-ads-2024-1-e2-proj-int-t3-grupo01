eb17cb32fbb3d4d5c447b9812904e4c558d564aa
Naiana Mendonça Coelho <144706915+naianamendonca@users.noreply.github.com>
2024-05-04 01:27:39 -0300

    Adicionar arquivos de projeto.

---
+++ gestlab-telagestor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gestlab_telagestor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
