88fcb1750f5d06e6f79a4910fc664df5e0f6b9e9
Naiana Mendonça Coelho <144706915+naianamendonca@users.noreply.github.com>
2024-05-04 01:41:15 -0300

    Delete gestlab-telagestor directory

--- gestlab-telagestor/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace gestlab_telagestor.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
