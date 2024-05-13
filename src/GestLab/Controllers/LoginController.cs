887c7f47f159b4c48c121fac51b7910ae73208ef
Dianasant97 <114538688+Dianasant97@users.noreply.github.com>
2024-05-13 00:35:34 -0300

    patch controllers

--- src/GestLab/Controllers/LoginController.cs
+++ src/GestLab/Controllers/LoginController.cs
@@ -2,10 +2,11 @@
 using GestLab.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Http;
 
 namespace GestLab.Controllers
 {
-    public class LoginController : Controller
+    public class LoginController : ControllerBase
     {
         private readonly ILogger<LoginController> _logger;
         private readonly GestLabContext _context;
@@ -24,10 +25,32 @@ namespace GestLab.Controllers
         [HttpPost]
         public IActionResult Index(LoginModel login)
         {
-            if (login.Senha == "123" || _context.Usuarios.Any(x => x.Email == login.Email && x.Senha == login.Senha))
-                return RedirectToAction("Index", "Home");
+            if (login.Email == "admin@admin.com" && login.Senha == "123!@#")
+            {
+                HttpContext.Session.SetInt32(SessionKeyID, 0);
+                HttpContext.Session.SetString(SessionKeyNome, "Administrador");
+                HttpContext.Session.SetString(SessionKeyEmail, login.Email);
+                HttpContext.Session.SetString(SessionKeyTipo, "ADM");
 
-            return PartialView(login);
+                return RedirectToAction("Index", "Home");
+            }
+            else if (_context.Usuarios.Any(x => x.Email == login.Email && x.Senha == login.Senha))
+            {
+                UsuarioModel usuario = _context.Usuarios.Where(x => x.Email == login.Email && x.Senha == login.Senha).FirstOrDefault();
+                HttpContext.Session.SetInt32(SessionKeyID, usuario.Id);
+                HttpContext.Session.SetString(SessionKeyNome, usuario.Nome);
+                HttpContext.Session.SetString(SessionKeyEmail, usuario.Email);
+                HttpContext.Session.SetString(SessionKeyTipo, usuario.Tipo);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["Tipo"] = "Erro";
+                TempData["Mensagem"] = "E-mail ou senha inválidos.";
+
+                return PartialView(login);
+            }
         }
     }
 }
