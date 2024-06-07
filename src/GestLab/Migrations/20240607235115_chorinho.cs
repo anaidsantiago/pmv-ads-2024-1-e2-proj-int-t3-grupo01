588521b98f49f14eef3f1f9ebee1a76d99fc13c8
João <joaovictor_afl@hotmail.com>
2024-06-07 20:59:31 -0300

    Publicaçaõ

---
+++ src/GestLab/Migrations/20240607235115_chorinho.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestLab.Migrations
+{
+    /// <inheritdoc />
+    public partial class chorinho : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
